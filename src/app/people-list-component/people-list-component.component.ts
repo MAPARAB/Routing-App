21019effa773b4e3ce4778895a071929dc1f55e8
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-11-07 15:22:21 +0530

    Adding the changes

--- src/app/people-list-component/people-list-component.component.ts
+++ src/app/people-list-component/people-list-component.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {Person} from './person'
 
 @Component({
   selector: 'app-people-list-component',
   templateUrl: './people-list-component.component.html',
   styleUrls: ['./people-list-component.component.css']
 })
-export class PeopleListComponentComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+export class PeopleListComponentComponent 
+{
+  people: Person[] = [
+    {name: 'Luke Skywalker', height: 177, weight: 70},
+    {name: 'Darth Vader', height: 200, weight: 100},
+    {name: 'Han Solo', height: 185, weight: 85},
+  ];
 
 }
