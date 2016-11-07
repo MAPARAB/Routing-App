9566ee19589faf6509599b7369eed1989fa6e352
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-11-07 20:08:02 +0530

    updated the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { PeopleServiceService } from './people-list-component/people-service.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  // HERE! This registers the PeopleService 
+  // now Angular 2 knows to inject it when required
+  providers: [PeopleServiceService]
+
 })
 export class AppComponent {
   title = 'app works hello all done via GIT!';
