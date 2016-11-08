0c2a8f4f577266201d82987e10024637d1eec32c
root <root@localhost.localdomain>
2016-11-08 12:20:42 +0530

    Adding new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { PeopleListComponentComponent } from './people-list-component/people-list-component.component';
 import { PeopleServiceService } from './people-list-component/people-service.service';
+import { PersonDetailsComponent } from './people-list-component/person-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleListComponentComponent
+    PeopleListComponentComponent,
+    PersonDetailsComponent
   ],
   imports: [
     BrowserModule,
