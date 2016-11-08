503facc666bc3fd3ecab098a80a46fbcd1053563
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-11-08 16:23:22 +0530

    updated people detail component and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PersonDetailsComponent } from './people-list-component/person-details.component';
 import { PeopleListComponentComponent } from './people-list-component/people-list-component.component';
 import { PeopleServiceService } from './people-list-component/people-service.service';
-import { PersonDetailsComponent } from './people-list-component/person-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleListComponentComponent,
-    PersonDetailsComponent
+    PersonDetailsComponent,
+    PeopleListComponentComponent
   ],
   imports: [
     BrowserModule,
